b5d79f7a1cd453ae49e69f7ff9e91cbf2066deee
Rey <reycrisbf@gmail.com>
2024-07-18 11:04:23 -0400

    Se hizo completo la vista historico.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "custom-shadow": "15px 11px 5px rgba(0, 0, 0, 1)",
+      },
+    },
+  },
+  plugins: [],
+};
